cef7135588d26cb7086d05d9517a32a2105d4943
Abi506 <abinandhan506@gmail.com>
2024-11-11 12:09:36 +0530

    version 7

--- src/components/apiUrl.js
+++ src/components/apiUrl.js
@@ -1,4 +1,4 @@
-const host=`https://personalwebsitebackend.vercel.app`
+const host=`https://personal-website-three-sandy-97.vercel.app`
 //const host='http://localhost:3000'
 export const taskPrioriterUrl=`${host}/taskPrioriter`
 export const watchLaterUrl=`${host}/videoWatchLater`
